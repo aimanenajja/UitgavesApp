f46796f1eca4e06a5c13da17cf34da57fd35943c
Aimane Najja <aimane.najja@gmail.com>
2019-11-24 22:19:26 +0100

    SQL Server LocalDB coupled to the app

--- UitgavesApp/App.xaml.cs
+++ UitgavesApp/App.xaml.cs
@@ -13,5 +13,6 @@ namespace UitgavesApp
     /// </summary>
     public partial class App : Application
     {
+        public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=UitgavesApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 }
--- UitgavesApp/MainWindow.xaml.cs
+++ UitgavesApp/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace UitgavesApp
 {
@@ -20,7 +23,51 @@ namespace UitgavesApp
             InitializeComponent();
             _uitgaves = new ObservableCollection<UitgaveItem>();
             dataGrid.ItemsSource = _uitgaves;
+            SeedGrid();
+        }
 
+        private void SeedGrid()
+        {
+            using (SqlConnection conn = new SqlConnection(App.ConnectionString))
+            {
+                var cmd = new SqlCommand
+                {
+                    CommandType = CommandType.Text,
+                    CommandText = @"SELECT Datum, Bedrag, Naam 
+                                    FROM Uitgaves
+                                    JOIN Categorieen
+                                        ON Uitgaves.CategorieId = Categorieen.Id",
+                    Connection = conn
+                };
+
+                try
+                {
+                    conn.Open();
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        var datum = ((DateTime) reader["Datum"]).ToString("dd MMMM yyyy");
+                        var bedrag = (double) reader["Bedrag"];
+                        var categorie = reader["Naam"].ToString();
+
+                        _uitgaves.Add(new UitgaveItem
+                        {
+                            Datum = datum,
+                            Uitgegeven = bedrag,
+                            Categorie = categorie
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                }
+            }
         }
 
         private void CbxCategorie_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -39,32 +86,103 @@ namespace UitgavesApp
 
         private void BtnToevoegen_Click(object sender, RoutedEventArgs e)
         {
-            var selectedCategorie = ((ComboBoxItem)cbxCategorie.SelectedValue).Content.ToString();
-            string categorieNaam;
-
-            if (selectedCategorie.Equals("Andere"))
+            using (SqlConnection conn = new SqlConnection(App.ConnectionString))
             {
-                if (string.IsNullOrEmpty(txtboxAndere.Text))
+                var cmd = new SqlCommand
                 {
-                    MessageBox.Show("Gelieve een categorienaam in te geven.");
-                    return;
+                    CommandType = CommandType.Text,
+                    CommandText = @"BEGIN
+                                       IF NOT EXISTS (SELECT * FROM Categorieen WHERE Naam = @Naam)
+                                       BEGIN
+                                           INSERT INTO Categorieen (Naam) VALUES (@Naam);
+                                       END
+                                    END",
+                    Connection = conn
+                };
+
+                var selectedCategorie = ((ComboBoxItem)cbxCategorie.SelectedValue).Content.ToString();
+                string categorieNaam;
+
+                if (selectedCategorie.Equals("Andere"))
+                {
+                    if (string.IsNullOrEmpty(txtboxAndere.Text))
+                    {
+                        MessageBox.Show("Gelieve een categorienaam in te geven.");
+                        return;
+                    }
+                    else
+                    {
+                        categorieNaam = txtboxAndere.Text;
+                    }
                 }
                 else
                 {
-                    categorieNaam = txtboxAndere.Text;
+                    categorieNaam = ((ComboBoxItem)cbxCategorie.SelectedItem).Content.ToString();
                 }
-            }
-            else
-            {
-                categorieNaam = ((ComboBoxItem)cbxCategorie.SelectedItem).Content.ToString();
-            }
 
-            _uitgaves.Add(new UitgaveItem
-            {
-                Uitgegeven = Convert.ToDouble(txtBoxUitgegeven.Text),
-                Datum = dtpDatum.SelectedDate.GetValueOrDefault().ToString("dd MMMM yyyy"),
-                Categorie = categorieNaam
-            });
+                var bedrag = Convert.ToDouble(txtBoxUitgegeven.Text);
+                var datum = dtpDatum.SelectedDate.GetValueOrDefault();
+
+                _uitgaves.Add(new UitgaveItem
+                {
+                    Uitgegeven = bedrag,
+                    Datum = datum.ToString("dd MMMM yyyy"),
+                    Categorie = categorieNaam
+                });
+
+                cmd.Parameters.AddWithValue("@Naam", categorieNaam);
+
+                try
+                {
+                    conn.Open();
+                    var modified = cmd.ExecuteScalar();
+
+                    int categorieId;
+
+                    if (modified != null)
+                    {
+                        cmd = new SqlCommand
+                        {
+                            CommandType = CommandType.Text,
+                            CommandText = "SELECT @@IDENTITY;",
+                            Connection = conn
+                        };
+
+                        categorieId = (int) cmd.ExecuteScalar();
+                    }
+                    else
+                    {
+                        cmd = new SqlCommand
+                        {
+                            CommandType = CommandType.Text,
+                            CommandText = "SELECT Id FROM Categorieen WHERE Naam = @Naam",
+                            Connection = conn
+                        };
+                        cmd.Parameters.AddWithValue("@Naam", categorieNaam);
+                        categorieId = (int) cmd.ExecuteScalar();
+                    }
+
+                    cmd = new SqlCommand
+                    {
+                        CommandType = CommandType.Text,
+                        CommandText = @"INSERT INTO Uitgaves (Datum, Bedrag, CategorieId) VALUES (@Datum, @Bedrag, @CategorieId);",
+                        Connection = conn
+                    };
+                    cmd.Parameters.AddWithValue("@Datum", datum);
+                    cmd.Parameters.AddWithValue("@Bedrag", bedrag);
+                    cmd.Parameters.AddWithValue("@CategorieId", categorieId);
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                }
+            }           
         }
 
         class UitgaveItem
@@ -167,6 +285,19 @@ namespace UitgavesApp
                 maandUitgegevenList.Add(maandJaarUitgegeven);
             }
             return maandUitgegevenList;
+        }
+
+        private void TxtBoxUitgegeven_KeyDown(object sender, KeyEventArgs e)
+        {
+            var numpadNummers = e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9;
+            var toetsenBordNummers = e.Key >= Key.D0 && e.Key <= Key.D9;
+            var komma = e.Key == Key.OemComma;
+
+            if (!numpadNummers && !toetsenBordNummers && !komma)
+            {
+                e.Handled = true;
+                MessageBox.Show($"{e.Key.ToString()} is forbidden");
+            }
         }
     }
 }
